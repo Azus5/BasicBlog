588bbef1c14710e82a1a7aa5385609b71cac7077
Abner Ferrante <abnerchoba@hotmail.com>
2020-06-21 06:12:17 -0300

    Basic API

---
+++ testeAPI/app/controllers/posts_controller.rb
@@ -0,0 +1,71 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+  before_action :require_authorization!, only: [:show, :update, :destroy]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    render json: @post
+  end
+
+  # POST /posts
+  def create
+    # "post_params.merge(user: current_user)" puts user id in Post 
+    @post = Post.new(post_params.merge(user: current_user))
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:title, :content, :user_id)
+    end
+
+    def require_authorization!
+      unless current_user == @post.user
+        render json: {}, status: :forbidden
+      end
+    end
+end
---
+++ testeAPI/config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :posts
+  match '*path', via: [:options], to: lambda {|_| [204, { 'Content-Type' => 'text/plain' }]}
+
+  # Session Related
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get 'logged_in', to: 'sessions#is_logged_in?'
+
+  #Post related
+  
+  resources :posts, only: [:index, :create, :destroy, :show, :update]
+  resources :users, only: [:create, :show, :index]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
